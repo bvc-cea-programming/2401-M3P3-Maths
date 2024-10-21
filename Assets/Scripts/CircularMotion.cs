c0363600509ae8e159434e2c776289d3614910ef
Jahnavi Patel <j.patel717@mybvc.ca>
2024-10-21 17:53:55 -0600

    Part 1 Circulation Motion

--- Assets/Scripts/CircularMotion.cs
+++ Assets/Scripts/CircularMotion.cs
@@ -7,12 +7,19 @@ public class CircularMotion : MonoBehaviour
     public Transform targetObject;
     
     public float xAmplitude = 2f;
-    public float yAmplitude = 2f;
+    public float yTime = 2f;
     public float speed = 5f;
     private void Animate()
     {
         //Add your code here
-        
+        yTime += Time.deltaTime * speed;
+
+        // Calculate the new position
+        float x = xAmplitude * Mathf.Sin(yTime);
+        float y = xAmplitude * Mathf.Cos(yTime);
+
+        // Set the new position
+        transform.localPosition = new Vector3(x, y, transform.localPosition.z);
     }
 
     private void Update()
