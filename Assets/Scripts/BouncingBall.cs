4eabc9ace3d81e5bd138cb0d8a5352f48e076429
flamypow <wytchen90s@gmail.com>
2024-10-21 15:52:11 -0600

    BouncingBall Done

--- Assets/Scripts/BouncingBall.cs
+++ Assets/Scripts/BouncingBall.cs
@@ -8,10 +8,18 @@ public class BouncingBall : MonoBehaviour
     
     public float amplitude = 3f;
     public float frequency = 2f;
+    private float startPosition;
+    private float currentPosition;
+
+    private void Start()
+    {
+        startPosition = targetObject.position.y;
+    }
     private void Animate()
     {
         //Add your code here
-        
+        currentPosition = startPosition + amplitude + Mathf.Abs(Mathf.Sin(frequency * Time.time));
+        targetObject.position = new Vector3(targetObject.position.x, currentPosition, targetObject.position.z);
     }
 
     private void Update()
