7bf35d2c6525c337fe1d79c5ea8739ab3e9fbb01
ToxicCanna <freedom22140@gmail.com>
2024-10-21 17:04:31 -0600

    Commit Cleanup

--- Assets/Scripts/ParametricShapes.cs
+++ Assets/Scripts/ParametricShapes.cs
@@ -8,10 +8,16 @@ public class ParametricShapes : MonoBehaviour
     public Transform targetObject;
     public float amplitude = 0.3f;
     public float speed = 5f;
+    public float time;
     private void Animate()
     {
+        time += 0.05f;
+        time %= 15;
         //Add your code here
-        
+        float x = amplitude * Mathf.Sin(time * speed) * time;
+        float y = amplitude * Mathf.Cos(time * speed) * time;
+
+        targetObject.localPosition = new Vector3(x, y, 0);
     }
 
     private void Update()
