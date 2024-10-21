1f401cfcccce16cf72174cb6d2742c10e7fe199e
Kyle.C <k.chudobiak271@mybvc.ca>
2024-10-21 17:16:12 -0600

    Bouncing Ball Done

--- Assets/Scripts/BouncingBall.cs
+++ Assets/Scripts/BouncingBall.cs
@@ -8,12 +8,13 @@ public class BouncingBall : MonoBehaviour
     
     public float amplitude = 3f;
     public float frequency = 2f;
+    private float posY;
     private void Animate()
     {
         //Add your code here
-        
+        posY = amplitude * Mathf.Sin(Time.time * frequency);
+        targetObject.localPosition = new Vector3(0, Mathf.Abs(posY), 0);
     }
-
     private void Update()
     {
         if(!targetObject) return;
