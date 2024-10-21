3fb0be5aa58f70eb54c6d5ec222726e02ea9d7c8
flamypow <wytchen90s@gmail.com>
2024-10-21 15:51:58 -0600

    CircularMotion Done

--- Assets/Scripts/CircularMotion.cs
+++ Assets/Scripts/CircularMotion.cs
@@ -9,10 +9,22 @@ public class CircularMotion : MonoBehaviour
     public float xAmplitude = 2f;
     public float yAmplitude = 2f;
     public float speed = 5f;
+    private float xPosition;
+    private float yPosition;
+    private float xStart;
+    private float yStart;
+
+    private void Start()
+    {
+        xStart = targetObject.position.x;
+        yStart = targetObject.position.y;
+    }
     private void Animate()
     {
         //Add your code here
-        
+        xPosition = xStart + xAmplitude * Mathf.Sin(speed * Time.time);
+        yPosition = yStart + yAmplitude * Mathf.Cos(speed * Time.time);
+        targetObject.position = new Vector3(xPosition, yPosition, targetObject.position.z);
     }
 
     private void Update()
