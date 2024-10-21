44f45167beeee7628cdf3ec7075bfc3192049ffe
Smooztime <chanakan.zoom@gmail.com>
2024-10-21 16:31:43 -0600

    Circular Motion

--- Assets/Scripts/CircularMotion.cs
+++ Assets/Scripts/CircularMotion.cs
@@ -1,24 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CircularMotion : MonoBehaviour
 {
     public Transform targetObject;
-    
+
     public float xAmplitude = 2f;
     public float yAmplitude = 2f;
     public float speed = 5f;
     private void Animate()
     {
         //Add your code here
-        
+        float positionX = xAmplitude * Mathf.Cos(speed * Time.time);
+        float positionY = yAmplitude * Mathf.Sin(speed * Time.time);
+
+        targetObject.localPosition = new Vector3(positionX, positionY, targetObject.localPosition.z);
     }
 
     private void Update()
     {
-        if(!targetObject) return;
-        
+        if (!targetObject) return;
+
         Animate();
     }
 }
