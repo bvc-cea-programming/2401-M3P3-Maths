57cf63a42080ff0518d972b400911a7223494c6b
deepanshu <deepanshu120100@gmail.com>
2024-10-21 16:31:45 -0600

    Circular Motion check push

--- Assets/Scripts/CircularMotion.cs
+++ Assets/Scripts/CircularMotion.cs
@@ -23,4 +23,11 @@ public class CircularMotion : MonoBehaviour
         
         Animate();
     }
+    //notes for me
+    //mathf.sin is for horizontal
+    //mathf.cos id for vertical 
+    //Xamplitude is for how much horizontal wider ,controls the motion around xaxis
+    //yamplitude is for how much vertical  wider ,controls the motion around yaxis
+    //if they both are same then it's a circle
+    
 }
