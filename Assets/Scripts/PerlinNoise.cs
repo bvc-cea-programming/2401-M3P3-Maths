70f97aa8dc06b489d3d4a86ad6d6f8d84e49251d
Jahnavi Patel <j.patel717@mybvc.ca>
2024-10-21 18:38:59 -0600

    Part 4 Perlin Noise

--- Assets/Scripts/PerlinNoise.cs
+++ Assets/Scripts/PerlinNoise.cs
@@ -1,17 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class PerlinNoise : MonoBehaviour
 {
     public Light targetLight;
-    
+
     public float speed = 3f;
     public float intensityMultiplier = 2f;
+    public Light pointLight;
+
+    private float timeOffset;
+
+
+
+     void start()
+    {
+        timeOffset = Random.Range(0f, 100f);
+    }
     private void Animate()
     {
         //Add your code here
-        
+
+        float x = Time.time * speed + timeOffset;
+
+        // Generate a Perlin noise value and scale it
+        float noiseValue = Mathf.PerlinNoise(x, 0f);
+        pointLight.intensity = noiseValue * intensityMultiplier; // Set light intensity
+
     }
 
     private void Update()
